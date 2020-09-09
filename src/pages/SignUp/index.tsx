9509d6d0527cf1628c166808f89b6e35623b3b90
Benício Barreto <benicio.daniel@gmail.com>
2020-09-09 15:17:47 -0300

    Added Yup lib. Started input validation

--- src/pages/SignUp/index.tsx
+++ src/pages/SignUp/index.tsx
@@ -1,6 +1,7 @@
-import React from 'react';
+import React, { useCallback } from 'react';
 import { FiMail, FiLock, FiUser, FiArrowLeft } from 'react-icons/fi';
 import { Form } from '@unform/web';
+import * as Yup from 'yup';
 import logoImg from '../../assets/logo.svg';
 
 import { Container, Content, Background } from './styles';
@@ -8,9 +9,25 @@ import Input from '../../components/input';
 import Button from '../../components/button';
 
 const SignUp: React.FC = () => {
-  function handleSubmit(formData: object): void {
+  const handleSubmit = useCallback(async (formData: object) => {
     console.log(formData);
-  }
+
+    try {
+      const schema = Yup.object().shape({
+        name: Yup.string().required('Nome obrigatório'),
+        email: Yup.string()
+          .required('E-mail obrigatório')
+          .email('Digite um e-mail válido'),
+        password: Yup.string().min(6, 'No mínimo 6 dígitos'),
+      });
+
+      await schema.validate(formData, {
+        abortEarly: false,
+      });
+    } catch (error) {
+      console.log(error);
+    }
+  }, []);
 
   return (
     <Container>
