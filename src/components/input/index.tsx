7b571ff15b67f292c87bd3acf25b4ee997c916ae
Benício Barreto <benicio.daniel@gmail.com>
2020-09-09 14:38:08 -0300

    Added condition to render border

--- src/components/input/index.tsx
+++ src/components/input/index.tsx
@@ -1,4 +1,10 @@
-import React, { InputHTMLAttributes, useEffect, useRef } from 'react';
+import React, {
+  InputHTMLAttributes,
+  useEffect,
+  useRef,
+  useState,
+  useCallback,
+} from 'react';
 import { IconBaseProps } from 'react-icons';
 import { useField } from '@unform/core';
 import { Container } from './styles';
@@ -9,22 +15,40 @@ interface InputProps extends InputHTMLAttributes<HTMLInputElement> {
 }
 
 const Input: React.FC<InputProps> = ({ name, icon: Icon, ...otherProps }) => {
+  const inputRef = useRef<HTMLInputElement>(null);
   // como se fosse o jQuery para maniplar diretamente o elemento do dom
-  const inputRef = useRef(null);
   const { fieldName, defaultValue, error, registerField } = useField(name);
+
+  const [isFocused, setIsFocused] = useState(false);
+  const [isFilled, setIsFilled] = useState(false);
+
+  const handleInputFocus = useCallback(() => {
+    setIsFocused(true);
+  }, []);
+
+  const handleInputBlur = useCallback(() => {
+    setIsFocused(false);
+    setIsFilled(!!inputRef.current?.value);
+  }, []);
 
   useEffect(() => {
     registerField({
       name: fieldName,
-      ref: inputRef.current,
+      ref: inputRef.current, // o elemento em si, ex <input></input>
       path: 'value', // document.querySelector('input').value -> valor do campo
     });
   }, [fieldName, registerField]);
 
   return (
-    <Container>
+    <Container isFocused={isFocused} isFilled={isFilled}>
       {Icon && <Icon size={20} />}
-      <input defaultValue={defaultValue} ref={inputRef} {...otherProps} />
+      <input
+        onFocus={handleInputFocus}
+        onBlur={handleInputBlur}
+        defaultValue={defaultValue}
+        ref={inputRef}
+        {...otherProps}
+      />
     </Container>
   );
 };
