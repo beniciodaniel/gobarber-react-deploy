ef25c1b1f58ba821db14ebecd9138c19ccb9a1ec
Benício Barreto <benicio.daniel@gmail.com>
2020-09-13 18:49:17 -0300

    Created authenticated routes

---
+++ src/routes/Route.tsx
@@ -0,0 +1,41 @@
+import React from 'react';
+import {
+  RouteProps as ReactDOMRouteProps,
+  Route as ReactDOMRoute,
+  Redirect,
+} from 'react-router-dom';
+
+import { useAuth } from '../context/AuthContext';
+
+interface RouteProps extends ReactDOMRouteProps {
+  isPrivate?: boolean;
+  component: React.ComponentType;
+}
+
+const Route: React.FC<RouteProps> = ({
+  isPrivate = false,
+  component: Component,
+  ...otherProps
+}) => {
+  const { user } = useAuth(); // se tiver dados dentro de user -> usuário está autenticado
+
+  return (
+    <ReactDOMRoute
+      {...otherProps}
+      render={({ location }) => {
+        return isPrivate === !!user ? (
+          <Component />
+        ) : (
+          <Redirect
+            to={{
+              pathname: isPrivate ? '/' : '/dashboard',
+              state: { from: location },
+            }}
+          />
+        );
+      }}
+    />
+  );
+};
+
+export default Route;
