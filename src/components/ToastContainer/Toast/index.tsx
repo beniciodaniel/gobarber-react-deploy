ebac4a7336f3b349acd0905e04595e6583418cbd
Benício Barreto <benicio.daniel@gmail.com>
2020-09-13 16:11:29 -0300

    Added react-spring. Implemented Toast animation

--- src/components/ToastContainer/Toast/index.tsx
+++ src/components/ToastContainer/Toast/index.tsx
@@ -12,6 +12,7 @@ import { Container } from './styles';
 
 interface ToastProps {
   message: ToastMessage;
+  style: Object;
 }
 
 const icons = {
@@ -20,7 +21,7 @@ const icons = {
   success: <FiCheckCircle size={24} />,
 };
 
-const Toast: React.FC<ToastProps> = ({ message }) => {
+const Toast: React.FC<ToastProps> = ({ message, style }) => {
   const { removeToast } = useToast();
 
   useEffect(() => {
@@ -35,7 +36,11 @@ const Toast: React.FC<ToastProps> = ({ message }) => {
   }, [removeToast, message.id]);
 
   return (
-    <Container type={message.type} hasDescription={!!message.description}>
+    <Container
+      style={style}
+      type={message.type}
+      hasDescription={!!message.description}
+    >
       {icons[message.type || 'info']}
 
       <div>
