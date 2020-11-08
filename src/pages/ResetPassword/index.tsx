62da43788d46a156a9e9a9346cc024286c1e8089
Benício Barreto <benicio.daniel@gmail.com>
2020-11-08 12:32:27 -0300

    Creating redefinition page (reset password)

---
+++ src/pages/ResetPassword/index.tsx
@@ -0,0 +1,92 @@
+import React, { useCallback, useRef } from 'react';
+import { FiLock } from 'react-icons/fi';
+import { Form } from '@unform/web';
+import { FormHandles } from '@unform/core';
+import * as Yup from 'yup';
+import { useHistory } from 'react-router-dom';
+import logoImg from '../../assets/logo.svg';
+
+import Input from '../../components/input';
+import Button from '../../components/button';
+import getValidationErrors from '../../utils/getValidationErrors';
+
+import { Container, Content, Background, AnimationContainer } from './styles';
+import { useToast } from '../../context/ToastContext';
+
+interface ResetPasswordFormData {
+  password: string;
+  password_confirmation: string;
+}
+
+const ResetPassword: React.FC = () => {
+  const formRef = useRef<FormHandles>(null); // para poder setar os erros nos campos do Form (Unform)
+
+  const { addToast } = useToast();
+  const history = useHistory();
+
+  const handleSubmit = useCallback(
+    async (formData: ResetPasswordFormData) => {
+      try {
+        formRef.current?.setErrors({}); // para sempre fazer a validação do zero
+
+        const schema = Yup.object().shape({
+          password: Yup.string().required('Senha obrigatória'),
+          password_confirmation: Yup.string().oneOf(
+            [Yup.ref('password'), undefined],
+            'Confirmação incorreta',
+          ),
+        });
+
+        await schema.validate(formData, {
+          abortEarly: false, // por padrão o Yup para no primeiro erro
+        });
+
+        history.push('/');
+      } catch (error) {
+        if (error instanceof Yup.ValidationError) {
+          const errors = getValidationErrors(error);
+
+          formRef.current?.setErrors(errors);
+          return;
+        }
+        // disparar um toast
+        addToast({
+          type: 'error',
+          title: 'Erro ao resetar senha',
+          description: 'Ocorreu um erro ao resetar sua senha, tente novamente',
+        });
+      }
+    },
+    [addToast, history],
+  );
+
+  return (
+    <Container>
+      <Content>
+        <AnimationContainer>
+          <img src={logoImg} alt="Logo" />
+
+          <Form ref={formRef} onSubmit={handleSubmit}>
+            <h1>Resetar senha</h1>
+            <Input
+              icon={FiLock}
+              name="password"
+              type="password"
+              placeholder="Nova senha"
+            />
+            <Input
+              icon={FiLock}
+              name="password_confirmation"
+              type="password"
+              placeholder="Confirmação da senha"
+            />
+            <Button type="submit">Alterar senha</Button>
+          </Form>
+        </AnimationContainer>
+      </Content>
+      <Background />
+    </Container>
+  );
+};
+
+export default ResetPassword;
