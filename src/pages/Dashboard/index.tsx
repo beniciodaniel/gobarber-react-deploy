40155516417f9afc78b63a4f299830de4082548e
Benício Barreto <benicio.daniel@gmail.com>
2020-11-09 16:05:36 -0300

    Creating Header component

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,5 +1,37 @@
 import React from 'react';
+import { FiPower } from 'react-icons/fi';
 
-const Dashboard: React.FC = () => <h1>Dashboard</h1>;
+import logoImg from '../../assets/logo.svg';
+import { useAuth } from '../../context/AuthContext';
+
+import { Container, Header, HeaderContent, Profile } from './styles';
+
+const Dashboard: React.FC = () => {
+  const { signOut, user } = useAuth();
+
+  console.log(user, 'Header Component');
+
+  return (
+    <Container>
+      <Header>
+        <HeaderContent>
+          <img src={logoImg} alt="GoBarber Logo" />
+
+          <Profile>
+            <img src={user.avatar_url} alt="Perfil" />
+            <div>
+              <span>Bem-vindo</span>
+              <strong>{user.name}</strong>
+            </div>
+          </Profile>
+
+          <button type="button" onClick={signOut}>
+            <FiPower />
+          </button>
+        </HeaderContent>
+      </Header>
+    </Container>
+  );
+};
 
 export default Dashboard;
